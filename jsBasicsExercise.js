702c9c185e9c37b374729f5af975590beb4bacde
Bryant Garcia <bryantgarcia@BCs-MacBook-Pro-2.local>
2021-02-22 13:21:48 -0600

    Complete JavaScript  Basics Exercise.

---
+++ jsBasicsExercise.js
@@ -0,0 +1,37 @@
+console.log(17);
+// The 5 main Data Types/Primitives are: Numbers, Strings, Boolean, Null, and Undefined. Symbols are also a Data Type, but are rarely used.
+
+// The 6 Operator Symbols are: (+ for Addition, - for Subtraction, * for Multiplication, / for Division, ** for Exponents, and % Modulus for remainders.)
+
+// Two ways to get a remainder of 1 from the number 5:
+console.log(5%2);
+console.log(5%4);
+
+console.log('I\'M MARRY POPPINS YA\'LL');
+
+console.log("Wanda" + "Vision");
+
+console.log("Bryant".length + "Garcia".length);
+
+console.log("Avengers Endgame"[15]);
+
+// BONUS
+console.log("\\");
+
+console.log(5**4-9);
+
+console.log("I".length + "Am".length % "Iron".length - "Man".length);
+
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("HAWKEYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+m=10;
+c=5
+u=m+c
+console.log(u);
